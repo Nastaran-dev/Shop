3ff3de1b93db2004cfa1e763481b432e3e64b597
Nastaran-dev <mohammadinastaran1384@gmail.com>
2026-02-23 16:23:16 +0330

    delet some images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
-"use client"
-import React, { useEffect, useState } from 'react'
+
 import HeroSection from './Section1/Section1'
 import NewArrivals from './section2/section2'
 import FabrixHero from './section3/section3'
@@ -10,30 +9,8 @@ import { SaleBanner } from './section7/section7'
 import { DiscoverMore } from './section8/section8'
 import { NewsletterSection } from './section9/section9'
 export default function Page() {
-   interface Item {
-  id: string
-  img: string
-  title: string
-}
-const [data, setData] = useState<Item[]>([])
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch("https://69536ae3a319a928023b6064.mockapi.io/data")
-      const json = await response.json()
-      setData(json)
-    }
-    fetchData()
-  }, [])
-  console.log(data)
   return (
     <div className='px-4 md:px-16'>
-      {data && data.map((item) => (
-        <div key={item.id}>
-         <img src={item.img} alt={item.title} />
-
-        </div>
-      ))}
       <HeroSection/>
      < NewArrivals/>
      <FabrixHero/>
